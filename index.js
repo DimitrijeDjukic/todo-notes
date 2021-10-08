67bdb4934dee0cfc2277f7a08c3d6cfe0744a16a
Dimitrije Djukic <dimitrijedjkc@gmail.com>
2021-10-08 09:27:48 +0200

    header

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-const button = document.getElementById("add-todo");
-button.addEventListener("click", handleClick);
+document.addEventListener("DOMContentLoaded", function(){ 
+
+    const button = window.document.getElementById("add-todo");
+    button.addEventListener("click", handleClick);
+
+
+    const input = window.document.getElementById("todo-note");
+    input.addEventListener("keydown", handleKeyDown)
+
+});
 
 function handleClick(event) {
-    const input = document.getElementById("todo-mote");
-    const list = document.getElementById("todo-list")
+    addToDo();
+}
+    
+function handleKeyDown(event) {
+    if(event.key === "Enter") {
+        addToDo()
+    }
+}
+
+function addToDo() {
+
+    const list = document.getElementById("todo-list");
+    const input = document.getElementById("todo-note");
+    const text = input.value;
 
     const item = document.createElement("li")
-    item.textContent = input.value;
+    item.textContent = text;
+    item.addEventListener("click", removeToDo)
+    input.value = "";
+    input.focus()
 
     list.appendChild(item);
-
-
-    const asd = [];
 }
+
+function removeToDo(event) {
+    const list = window.document.getElementById("todo-list")
+    list.removeChild(event.target)
+}
+
+ 
